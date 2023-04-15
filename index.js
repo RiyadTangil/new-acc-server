dffa1d8ae207f59b030c0c0affdff7eb62275607
RiyadTangil <riyadkhanxp2@gmail.com>
2023-04-15 10:38:07 +0600

    doen

--- index.js
+++ index.js
@@ -57,20 +57,20 @@ client.connect((err) => {
     });
   });
 
-  app.post("/addOrder", (req, res) => {
-    const orders = req.body;
-    ordersCollection.insertOne(orders).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
+  //   app.post("/addOrder", (req, res) => {
+  //     const orders = req.body;
+  //     ordersCollection.insertOne(orders).then((result) => {
+  //       res.send(result.insertedCount > 0);
+  //     });
+  //   });
 
-  app.post("/addAdmin", (req, res) => {
-    const admin = req.body;
-    console.log(admin);
-    adminCollection.insertOne(admin).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
+  //   app.post("/addAdmin", (req, res) => {
+  //     const admin = req.body;
+  //     console.log(admin);
+  //     adminCollection.insertOne(admin).then((result) => {
+  //       res.send(result.insertedCount > 0);
+  //     });
+  //   });
 
   app.get("/isAdmin?", (req, res) => {
     const email = req.query.email;
@@ -79,59 +79,59 @@ client.connect((err) => {
     });
   });
 
-  app.post("/addService", (req, res) => {
-    const file = req.files.file;
-    const name = req.body.name;
-    const description = req.body.description;
-    const price = req.body.price;
-    const newImg = file.data;
-    const encImg = newImg.toString("base64");
+  //   app.post("/addService", (req, res) => {
+  //     const file = req.files.file;
+  //     const name = req.body.name;
+  //     const description = req.body.description;
+  //     const price = req.body.price;
+  //     const newImg = file.data;
+  //     const encImg = newImg.toString("base64");
 
-    var image = {
-      contentType: file.mimetype,
-      size: file.size,
-      img: Buffer.from(encImg, "base64"),
-    };
-    console.log(name, description, image, price);
-    serviceCollection
-      .insertOne({ name, description, image, price })
-      .then((result) => {
-        res.send(result.insertedCount > 0);
-      });
-  });
+  //     var image = {
+  //       contentType: file.mimetype,
+  //       size: file.size,
+  //       img: Buffer.from(encImg, "base64"),
+  //     };
+  //     console.log(name, description, image, price);
+  //     serviceCollection
+  //       .insertOne({ name, description, image, price })
+  //       .then((result) => {
+  //         res.send(result.insertedCount > 0);
+  //       });
+  //   });
 
-  app.post("/addReview", (req, res) => {
-    const review = req.body;
+  //   app.post("/addReview", (req, res) => {
+  //     const review = req.body;
 
-    console.log("review", review);
+  //     console.log("review", review);
 
-    reviewCollection.insertOne(review).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
+  //     reviewCollection.insertOne(review).then((result) => {
+  //       res.send(result.insertedCount > 0);
+  //     });
+  //   });
 
-  app.patch("/update/:id", (req, res) => {
-    const newCondition = req.body.status;
-    console.log(newCondition);
-    ordersCollection
-      .updateOne(
-        { _id: ObjectId(req.params.id) },
+  //   app.patch("/update/:id", (req, res) => {
+  //     const newCondition = req.body.status;
+  //     console.log(newCondition);
+  //     ordersCollection
+  //       .updateOne(
+  //         { _id: ObjectId(req.params.id) },
 
-        {
-          $set: { status: newCondition },
-        }
-      )
-      .then((result) => {
-        res.send(result.modifiedCount > 0);
-      });
-  });
-  app.delete("/delete/:id", (req, res) => {
-    serviceCollection
-      .deleteOne({ _id: ObjectId(req.params.id) })
-      .then((result) => {
-        res.send(result.deletedCount > 0);
-      });
-  });
+  //         {
+  //           $set: { status: newCondition },
+  //         }
+  //       )
+  //       .then((result) => {
+  //         res.send(result.modifiedCount > 0);
+  //       });
+  //   });
+  //   app.delete("/delete/:id", (req, res) => {
+  //     serviceCollection
+  //       .deleteOne({ _id: ObjectId(req.params.id) })
+  //       .then((result) => {
+  //         res.send(result.deletedCount > 0);
+  //       });
+  //   });
 });
 
 app.listen(port);
